795c9a87db4a4adf65abac9354e0e1c7a7c0d9e0
Alberto Basalo <albertobasalo@hotmail.com>
2015-03-06 10:39:47 +0100

    paso a github

--- api/encuestas.js
+++ api/encuestas.js
@@ -1,6 +1,7 @@
 var encuestas = require('../data/encuestas.js');
 var MongoClient = require('mongodb').MongoClient;
-
+var mongoUrl = "mongodb://localhost:27017/encuesta";
+var mongoCol = "encuesta";
 
 exports.enrutar = function (router) {
     var rutaEncuestas = router.route('/api/encuestas/');
@@ -8,9 +9,9 @@ exports.enrutar = function (router) {
 
     rutaEncuestas
         .get(function (peticion, respuesta) {
-            MongoClient.connect("mongodb://localhost:27017/encuesta", function (err, db) {
+            MongoClient.connect(mongoUrl, function (err, db) {
                 if (!err) {
-                    var collection = db.collection('encuesta');
+                    var collection = db.collection(mongoCol);
                     collection.find().toArray(function (err, result) {
                         respuesta.json(result);
                     });
@@ -18,12 +19,10 @@ exports.enrutar = function (router) {
                     console.log(err);
                 }
             });
-            //respuesta.json(encuestas.selectAll());
         }).post(function (peticion, respuesta) {
-            // Rellenar una encuesta
-            MongoClient.connect("mongodb://localhost:27017/encuesta", function (err, db) {
+            MongoClient.connect(mongoUrl, function (err, db) {
                 if (!err) {
-                    var collection = db.collection('encuesta');
+                    var collection = db.collection(mongoCol);
                     collection.insert(peticion.body, function(err, result) {
                         respuesta.json(result);
                     });
