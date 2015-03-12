95a843446127f23228694cd6cb29720698241cfe
Alberto Basalo <albertobasalo@hotmail.com>
2015-03-12 14:26:18 +0100

    singlepage

--- static/LandingCtrl.js
+++ static/LandingCtrl.js
@@ -1,32 +1,57 @@
 (function () {
-	"use strict";
+    "use strict";
 
-	function controlador($stateParams, encuestasDataService, socketFactory) {
-		var vm = this;
-		socketFactory.connect();
-		
-		socketFactory.on('wellcome', function (data) {
-			console.log("Me han recibido bien: " + JSON.stringify(data));
-			updateMetrics();
-		});
+    function controlador(encuestasDataService, socketFactory) {
+        var vm = this;
 
-		socketFactory.on('updateTutti', function (data) {
-			console.log("Algo ha cambiado, recargando.... ");
-			updateMetrics();
-		});
-
-		function updateMetrics() {
-			encuestasDataService.getPreguntas().then(function (preguntas) {
-				vm.preguntas = preguntas;
-				vm.preguntas.forEach(function (pregunta) {
-					pregunta.encuestas = encuestasDataService.getCampoEncuestas(pregunta.campo);
-				});
+        vm.haVotado = false;
+        vm.ultimaActualizacion = new Date();
+        
+        
+        encuestasDataService.getPreguntas().then(function (preguntas) {
+			vm.preguntas = preguntas;
+			vm.preguntas.forEach(function (pregunta) {
+				pregunta.respuestas = encuestasDataService.getRespuestas(pregunta.id);
 			});
-		}
-	}
+		});
+        
+        vm.encuesta = encuestasDataService.newEncuesta();
 
-	angular
-		.module('encuestas')
-		.controller("LandingCtrl", controlador);
+        vm.enviarEncuesta = function () {
+            vm.preguntas.forEach(function (pregunta) {
+                vm.encuesta[pregunta.campo] = pregunta.respuesta;
+            });
+            encuestasDataService.postEncuestas(vm.encuesta);
+            socketFactory.connect();
+            socketFactory.on('wellcome', function (data) {
+                console.log("Me han recibido bien: " + JSON.stringify(data));
+            });
+
+            socketFactory.on('updateTutti', function (data) {
+                console.log("Algo ha cambiado, recargando.... ");
+                updateMetrics();
+            });
+            socketFactory.emit("postedData", {
+                date: new Date()
+            });
+            vm.haVotado = true;
+        }
+
+
+
+        function updateMetrics() {
+            encuestasDataService.getPreguntas().then(function (preguntas) {
+                vm.preguntas = preguntas;
+                vm.preguntas.forEach(function (pregunta) {
+                    pregunta.encuestas = encuestasDataService.getCampoEncuestas(pregunta.campo);
+                    vm.ultimaActualizacion = new Date();
+                });
+            });
+        }
+    }
+
+    angular
+        .module('encuestas')
+        .controller("LandingCtrl", controlador);
 
 }());
--- static/app.js
+++ static/app.js
@@ -2,28 +2,7 @@
     "use strict";
 
     angular
-        .module('encuestas', ['ngResource', 'ui.router']);
+        .module('encuestas', ['ngResource']);
 
-
-    function statesConfig($stateProvider, $urlRouterProvider, $locationProvider) {
-              
-        $urlRouterProvider.otherwise('/');
-
-        $stateProvider
-            .state('landing', {
-                url: '/',
-                templateUrl: 'landing.html',
-                controller: 'LandingCtrl as vm'
-            })
-            .state('nueva-encuesta', {
-                url: '/nueva',
-                templateUrl: 'nueva.html',
-                controller: 'NuevaCtrl as vm'
-            });
-		$locationProvider.html5Mode(true);
-    }
-
-    angular
-        .module("encuestas")
-        .config(statesConfig);
+    
 }());
